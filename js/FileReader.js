a791f5be754e779024fa43dcd6ba450a03e383e1
Thomas Souza <thomas.souza@deliverit.com.br>
2019-09-09 17:21:10 -0300

    Modifica leitura de arquivos para corresponder a url e ajusta interface

--- js/FileReader.js
+++ js/FileReader.js
@@ -1,24 +1,42 @@
 class TxtReader {
+
   constructor(_in, _out) {
     this.input = _in;
     this.output = _out;
     this.text = "";
 
-    this.input.addEventListener("change", () => {
-      this.read();
+    this.files = {
+      'teste.txt' : 'teste',
+      'desafio01.txt' : 'desafio_01',
+      'desafio02.txt' : 'desafio_02',
+      'desafio03.txt' : 'desafio_03'
+    };
+
+    for (var index in this.files) {
+      this.input.options[this.input.options.length] = new Option(this.files[index], index);
+    }
+
+    this.input.addEventListener("change", (event) => {
+      this.read(event);
     });
   }
 
-  read() {
-    if (this.input.files && this.input.files[0]) {
-      var reader = new FileReader();
+  read(event) {
+    var request = new XMLHttpRequest();
+      request.open('GET', './assemblyCode/' + event.target.value, true);
+      request.send(null);
+
       var out = this.output;
 
-      reader.addEventListener("load", function(e) {
-        this.text = out.textContent = e.target.result;
-        out.focus();
-      });
-      reader.readAsBinaryString(this.input.files[0]);
-    }
+      request.onreadystatechange = function () {
+          if (request.readyState === 4 && request.status === 200) {
+              var type = request.getResponseHeader('Content-Type');
+
+              if (type.indexOf("text") !== 1) {
+                this.text = out.textContent = request.responseText;
+                out.focus();
+              }
+          }
+      }
   }
 }
